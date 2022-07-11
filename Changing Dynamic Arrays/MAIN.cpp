e28151e2bd2abea259dc67c1b1d2a7289802f2b4
K0ndrich <123kondrich@gmail.com>
2022-07-11 13:52:46 +0300

    Добавил функции заполнения и вывода в консоль динамического массива.

--- Changing Dynamic Arrays/MAIN.cpp
+++ Changing Dynamic Arrays/MAIN.cpp
@@ -1,5 +1,34 @@
-
-void main() 
+#include<iostream>
+//‘ункци€ заполн€ет все €чейки динамического массива
+void FillArray(int* const arr, int& size)
 {
-	int a;
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+//‘ункци€ ввыводит динамический массви на екран
+void ShowArray(const int* const arr, int& size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << "  ";
+	}
+	std::cout << std::endl;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "RU");
+	srand(time(NULL));
+
+
+
+
+
+
+
+
+
+
 }
