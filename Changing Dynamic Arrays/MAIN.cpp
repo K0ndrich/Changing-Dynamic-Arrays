96ac1dc2ab9da97821bd3b4a3aef1613c7a5eed0
K0ndrich <123kondrich@gmail.com>
2022-07-11 14:00:40 +0300

    Копировал один динамический массив в другой

--- Changing Dynamic Arrays/MAIN.cpp
+++ Changing Dynamic Arrays/MAIN.cpp
@@ -17,18 +17,35 @@ void ShowArray(const int* const arr, int& size)
 	std::cout << std::endl;
 }
 
+void CopyArray(int*& arr1, int *arr2, int const& size)
+{
+
+}
 void main()
 {
 	setlocale(LC_ALL, "RU");
 	srand(time(NULL));
 
+	int size = 5;
+	int* arr1 = new int[size];
+	int* arr2 = new int[size];
 
+	FillArray(arr1, size);
+	FillArray(arr2, size);
+	ShowArray(arr1, size);
+	ShowArray(arr2, size);
+	std::cout << "==================================" << std::endl;
 
+	delete[]arr1;
+	arr1 = new int[size];
 
-
-
-
-
-
+	for (int i = 0; i < size; i++)
+	{
+		arr1[i] = arr2[i];
+	}
+	ShowArray(arr1, size);
+	ShowArray(arr2, size);
+	delete[]arr1;
+	delete[]arr2;
 
 }
